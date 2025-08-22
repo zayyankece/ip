5684cadfe1dbf14e37fa7d27bb5df33ee9a79826
zayyankece <mzayyanputra@gmail.com>
2025-08-22 11:18:44 +0800

    Add mark unmark feature

--- src/main/java/Joko.java
+++ src/main/java/Joko.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Joko {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int taskCount = 0;
 
         System.out.println("____________________________________________________________");
@@ -13,6 +13,9 @@ public class Joko {
 
         while (true) {
             String input = sc.nextLine();
+            String[] inputParts = input.split(" ", 2);
+            String command = inputParts[0];
+
             if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println(" Bye. Hope to see you again soon!");
@@ -20,12 +23,37 @@ public class Joko {
                 break;
             } else if (input.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + "." + taskList[i]);
                 }
                 System.out.println("____________________________________________________________");
+            } else if (command.equals("mark") || command.equals("unmark")) {
+                try {
+                    int index = Integer.parseInt(inputParts[1]) - 1;
+                    if (index < 0 || index >= taskCount) {
+                        System.out.println("Invalid task number.");
+                        continue;
+                    }
+                    if (command.equals("mark")) {
+                        taskList[index].isDone = true;
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   " + taskList[index]);
+                        System.out.println("____________________________________________________________");
+                    } else {
+                        taskList[index].isDone = false;
+                        System.out.println("____________________________________________________________");
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("   " + taskList[index]);
+                        System.out.println("____________________________________________________________");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Please type a valid input: <command> <task number>");
+                }
+
             } else {
-                taskList[taskCount] = input;
+                taskList[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + input);
@@ -34,5 +62,24 @@ public class Joko {
         }
 
         sc.close();
+    }
+
+    static class Task {
+        protected final String desc;
+        protected boolean isDone;
+
+        public Task(String desc) {
+            this.desc = desc;
+            this.isDone = false;
+        }
+
+        public String getStatus() {
+            return (isDone ? "[X] " : "[ ] ");
+        }
+
+        @Override
+        public String toString() {
+            return getStatus() + desc;
+        }
     }
 }
