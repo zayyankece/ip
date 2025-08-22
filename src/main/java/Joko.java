a5df2bd7f212c50213397a71ca38928598ebcf5b
zayyankece <mzayyanputra@gmail.com>
2025-08-22 10:26:06 +0800

    Add echo feature

--- src/main/java/Joko.java
+++ src/main/java/Joko.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Joko {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Joko");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+
+            }
+        }
+
+        sc.close();
     }
 }
