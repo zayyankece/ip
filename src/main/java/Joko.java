5364c21fed6315afdfdbbf2d74935e699408d7b3
zayyankece <mzayyanputra@gmail.com>
2025-08-22 10:38:19 +0800

    Add list and add feature

--- src/main/java/Joko.java
+++ src/main/java/Joko.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Joko {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] taskList = new String[100];
+        int taskCount = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Joko");
@@ -16,11 +18,18 @@ public class Joko {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + "." + taskList[i]);
+                }
+                System.out.println("____________________________________________________________");
             } else {
+                taskList[taskCount] = input;
+                taskCount++;
                 System.out.println("____________________________________________________________");
-                System.out.println(" " + input);
+                System.out.println(" added: " + input);
                 System.out.println("____________________________________________________________");
-
             }
         }
 
